7f042677c3a534ade9b291b408db6734009279ac
codeavenger1 <shivakansagara@gmail.com>
2020-05-17 17:28:16 -0700

    finished olympic rings; almost done with robot race

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -8,34 +8,51 @@ public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One
 	// robot should draw one ring simultaneously with the other 4 robots.
 	public static void main(String[] args) {
-		Robot blue = new Robot(350, 100);
-		Robot yellow = new Robot(500, 400);
-		Robot black = new Robot(650, 100);
-		Robot green = new Robot(800, 400);
-		Robot red = new Robot(950, 100);
-		
-		blue.setSpeed(10);
-		yellow.setSpeed(10);
-		black.setSpeed(10);
-		green.setSpeed(10);
-		red.setSpeed(10);
-		
-		blue.penDown();
-		yellow.penDown();
-		black.penDown();
-		green.penDown();
-		red.penDown();
-		
-		blue.setPenColor(Color.BLUE);
-		yellow.setPenColor(Color.YELLOW);
-		black.setPenColor(Color.BLACK);
-		green.setPenColor(Color.GREEN);
-		red.setPenColor(Color.RED);
-		
-		blue.hide();
-		yellow.hide();
-		black.hide();
-		green.hide();
-		red.hide();
+		Robot[] robArray = new Robot[5];
+		Thread[] threadArray = new Thread[5];
+
+		robArray[0] = new Robot(350, 150);
+		robArray[1] = new Robot(450, 300);
+		robArray[2] = new Robot(550, 150);
+		robArray[3] = new Robot(650, 300);
+		robArray[4] = new Robot(750, 150);
+
+		robArray[0].hide();
+		robArray[1].hide();
+		robArray[2].hide();
+		robArray[3].hide();
+		robArray[4].hide();
+
+		robArray[0].setSpeed(10);
+		robArray[1].setSpeed(10);
+		robArray[2].setSpeed(10);
+		robArray[3].setSpeed(10);
+		robArray[4].setSpeed(10);
+
+		robArray[0].penDown();
+		robArray[1].penDown();
+		robArray[2].penDown();
+		robArray[3].penDown();
+		robArray[4].penDown();
+
+		robArray[0].setPenColor(Color.BLUE);
+		robArray[1].setPenColor(Color.YELLOW);
+		robArray[2].setPenColor(Color.BLACK);
+		robArray[3].setPenColor(Color.GREEN);
+		robArray[4].setPenColor(Color.RED);
+
+		for (int i = 0; i < 5; i++) {
+			Robot rob = robArray[i];
+			
+			threadArray[i] = new Thread(() -> {
+				for (int j = 0; j < 360; j++) {
+					rob.setSpeed(10);
+					rob.turn(1);
+					rob.move(2);
+				}
+			});
+			
+			threadArray[i].start();
+		}
 	}
 }
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,50 @@
 package _02_Advanced_Robot_Race;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
-	// Re-do the robot race recipe from level 3 module 0. 
+
+	// Re-do the robot race recipe from level 3 module 0.
 	// This time, use threads to make all of the robots go at the same time.
+	public static void main(String[] args) {
+		Random rand = new Random();
+		
+
+		Robot[] rob = new Robot[5];
+		Thread[] threadArray = new Thread[5];
+
+		rob[0] = new Robot(70, 650);
+		rob[1] = new Robot(170, 650);
+		rob[2] = new Robot(270, 650);
+		rob[3] = new Robot(370, 650);
+		rob[4] = new Robot(470, 650);
+		
+		
+		for (int i = 0; i < 5; i++) {
+			Robot r = rob[i];
+			int j = i;
+			threadArray[i] = new Thread(() -> {
+				boolean go = true;
+				while (go) {
+						r.move(rand.nextInt(50));
+						if (r.getY()<= 0) {
+							go = false;
+						}
+						
+				}
+				if(go == false) {
+					int winner = j;
+					JOptionPane.showMessageDialog(null, "Robot " + winner + " won!");
+					rob[winner].sparkle();
+				}
+			});
+
+			threadArray[i].start();
+		}
+
+	}
 }
